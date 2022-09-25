fd832ca66ae7b280b96d520334da388db21e477d
Tolga Duman <tolga387@hotmail.com>
2022-09-25 03:16:08 +0300

    Data Types

---
+++ datatypes/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        double sayi = 12.5;
+        sayi = 1399999999;
+
+        char karakter = 'A';
+
+        boolean dogruMu = false;
+        System.out.println(karakter);
+    }
+}
